1e6cf0ad99a9e9f18987ac3b4ddbf0a24e22beaf
Anahit Harutyunyan <32642867+harutyunyananahit@users.noreply.github.com>
2022-12-05 14:30:07 +0400

    Initial commit

---
+++ test/ses-email-receiving-pipeline-cdk-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SesEmailReceivingPipelineCdkApp from '../lib/ses-email-receiving-pipeline-cdk-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ses-email-receiving-pipeline-cdk-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SesEmailReceivingPipelineCdkApp.SesEmailReceivingPipelineCdkAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
